f417523706b2e66f230364fd6e33ea27e173f7ea
Chiu <shivanipatil1511@gmail.com>
2021-06-17 20:58:59 +0530

    heruko domain chnges

--- ekisan/settings.py
+++ ekisan/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 # DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','ekisanfarmersportal.herokuapp.com']
 
 # Application definition
 
